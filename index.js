b97054e27159bf99b47ad488e57c0517f36ede40
senthur-003 <senthur.general@gmail.com>
2025-03-11 15:47:00 +0530

    firstcommit

---
+++ index.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const sql = require('mssql');
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+require('dotenv').config();
+
+
+const db = {
+    server: process.env.DB_SERVER,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    port: Number(process.env.DB_PORT),
+    options: {
+        encrypt: true,
+        trustServerCertificate: true,
+        enableArithAbort: true,
+    }
+}
+Port = 3000;
+const poolPromise = new sql.ConnectionPool(db)
+    .connect()
+    .then(pool => {
+        console.log('Database Connected successfully');
+        return pool;
+    })
+    .catch(err => console.log('Connection refused due to :', err));
+
+
+
+app.get('/user', async (req, res) => {
+    try {
+        const pool = await poolPromise;
+        const users = await pool.request()
+            .query(`select * from LOGIN_DETAILS`);
+        res.status(201).send({ message: 'user details got successfully' });
+    }
+    catch (err) {
+        res.status(500).send({ message: 'Error Creating item', error: err.message });
+    }
+});
+
+
+app.listen(Port, () => {
+    console.log(`Server is running in port : ${Port}`);
+});
