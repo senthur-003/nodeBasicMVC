f8e014ec741d0f21d9287fb06dc36ed731287eb5
senthur-003 <senthur.general@gmail.com>
2025-03-14 12:24:57 +0530

    unwanted code were removed

--- index.js
+++ index.js
@@ -11,29 +11,8 @@ app.use(cors());
 require('dotenv').config();
 
 
-// const db = {
-//     server: process.env.DB_SERVER,
-//     user: process.env.DB_USER,
-//     password: process.env.DB_PASSWORD,
-//     database: process.env.DB_DATABASE,
-//     port: Number(process.env.DB_PORT),
-//     options: {
-//         encrypt: true,
-//         trustServerCertificate: true,
-//         enableArithAbort: true,
-//     }
-// }
 Port = 3000;
 
-
-
-// const poolPromise = new sql.ConnectionPool(db)
-//     .connect()
-//     .then(pool => {
-//         console.log('Database Connected successfully');
-//         return pool;
-//     })
-//     .catch(err => console.log('Connection refused due to :', err));
 const poolPromise = require('./db');
 
 const userRoutes = require('./routes/userRoute');
@@ -44,7 +23,7 @@ app.get('/user', async (req, res) => {
         const pool = await poolPromise;
         const users = await pool.request()
             .query(`select * from LOGIN_DETAILS`);
-        res.status(201).send({ message: 'user details got successfully' });
+        res.status(200).send({ message: 'user details got successfully' });
     }
     catch (err) {
         res.status(500).send({ message: 'Error Creating item', error: err.message });
